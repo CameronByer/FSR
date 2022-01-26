a5cac00c75d424f68d1db4ec60a79a4b49814b85
CameronByer <chunecake@gmail.com>
2022-01-25 22:01:00 -0500

    Revert "Change to isometric view"
    This reverts commit ce967558a12fe8b4fdb6d0645c153b22f517326a.

--- FSR.py
+++ FSR.py
@@ -37,9 +37,6 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("FSR")
 clock = pygame.time.Clock() ## For syncing the FPS
 
-def mixcolors(a, b):
-    return ((a[0]+b[0])//2, (a[1]+b[1])//2, (a[2]+b[2])//2)
-
 class Rat:
 
     def __init__(self, x, y):
@@ -70,8 +67,9 @@ class Rat:
     def distancetopoint(self, x, y):
         return ((self.x-x)**2+(self.y-y)**2)**0.5
 
-    def draw(self, camerax, cameray, tileheight):
-        pygame.draw.circle(screen, BROWN, (self.x-camerax-tileheight, self.y-cameray-tileheight), self.radius)
+    def draw(self, camerax, cameray):
+        pygame.draw.circle(screen, BROWN, (self.x-camerax, self.y-cameray), self.radius)
+        self.drawhealthbar(WIDTH-110, HEIGHT-30)
 
     def drawhealthbar(self, x, y, width=100, height=20, border=3):
         pygame.draw.rect(screen, BLACK, (x, y, width, height))
@@ -124,7 +122,7 @@ class Map:
     def __init__(self, sizex, sizey):
         self.sizex = sizex
         self.sizey = sizey
-        self.tiles = [[Tile("Void", True, False, 0) for y in range(sizey)] for x in range(sizex)]
+        self.tiles = [[Tile("Void", True, False) for y in range(sizey)] for x in range(sizex)]
         self.items = {}
 
     def additem(self, item, x, y):
@@ -136,25 +134,23 @@ class Map:
             rely = y + diry*(i-radius)
             for j in range(radius+1):
                 if j<water_radius and i>radius-water_radius and i<(length+2*radius-(radius-water_radius)):
-                    tile1 = Tile("Sewer", False, True, -10, 3)
-                    tile2 = Tile("Sewer", False, True, -10, 3)
+                    tile1 = Tile("Sewer", False, True, 3)
+                    tile2 = Tile("Sewer", False, True, 3)
                 elif j<radius and i>0 and i<length+2*radius:
-                    tile1 = Tile("Stone", False, False, 0, 2)
-                    tile2 = Tile("Stone", False, False, 0, 2)
+                    tile1 = Tile("Stone", False, False, 2)
+                    tile2 = Tile("Stone", False, False, 2)
                 else:
-                    tile1 = Tile("Wall", True, False, 0, 1)
-                    tile2 = Tile("Wall", True, False, 0, 1)
+                    tile1 = Tile("Wall", True, False, 1)
+                    tile2 = Tile("Wall", True, False, 1)
                 if tile1.priority >= self.tiles[relx+diry*j][rely+dirx*j].priority:
                     self.tiles[relx+diry*j][rely+dirx*j] = tile1
                 if tile2.priority >= self.tiles[relx-diry*j][rely-dirx*j].priority:
                     self.tiles[relx-diry*j][rely-dirx*j] = tile2
 
-    def draw(self, camerax, cameray, rat):
+    def draw(self, camerax, cameray):
         for x in range(self.sizex):
             for y in range(self.sizey):
                 self.tiles[x][y].draw(x*BLOCKPIXELS, y*BLOCKPIXELS, camerax, cameray)
-                if rat.x >= x*BLOCKPIXELS and rat.x < (x+1)*BLOCKPIXELS and rat.y >= y*BLOCKPIXELS and rat.y < (y+1)*BLOCKPIXELS:
-                    rat.draw(camerax, cameray, self.tiles[x][y].height)
         for item, pos in self.items.items():
             item.draw(pos[0], pos[1], camerax, cameray)
             
@@ -179,30 +175,17 @@ class Map:
 
 class Tile:
 
-    def __init__(self, style, solid, water, height, priority = 0):
+    def __init__(self, style, solid, water, priority = 0):
         self.style = style
         self.solid = solid
         self.water = water
-        self.height = height
         self.priority = priority
     
     def additem(self, item, x, y):
         self.items[item] = (x, y)
 
     def draw(self, x, y, camerax, cameray):
-        pygame.draw.rect(screen, TILE_COLORS[self.style], (x-camerax-self.height, y-cameray-self.height, BLOCKPIXELS, BLOCKPIXELS))
-        self.drawsides(x, y, camerax, cameray, 10)
-
-    def drawsides(self, x, y, camerax, cameray, length):
-        color = mixcolors(TILE_COLORS[self.style], BLACK)
-        pygame.draw.polygon(screen, color, ((x-camerax-self.height+BLOCKPIXELS, y-cameray-self.height), #TOP RIGHT of tile
-                            (x-camerax-self.height+BLOCKPIXELS+length, y-cameray-self.height+length), 
-                            (x-camerax-self.height+BLOCKPIXELS+length, y-cameray-self.height+BLOCKPIXELS+length),
-                            (x-camerax-self.height+length, y-cameray-self.height+BLOCKPIXELS+length),
-                            (x-camerax-self.height, y-cameray-self.height+BLOCKPIXELS),
-                            (x-camerax-self.height+BLOCKPIXELS, y-cameray-self.height+BLOCKPIXELS)
-                        ))
-        
+        pygame.draw.rect(screen, TILE_COLORS[self.style], (x-camerax, y-cameray, BLOCKPIXELS, BLOCKPIXELS))
 
 class Item:
 
@@ -257,8 +240,8 @@ while running:
     camerax = r.x - WIDTH//2
     cameray = r.y - HEIGHT//2
     screen.fill(BLACK)
-    m.draw(camerax, cameray, r)
-    r.drawhealthbar(WIDTH-110, HEIGHT-30)
+    m.draw(camerax, cameray)
+    r.draw(camerax, cameray)
 
     pygame.display.flip()       
 
