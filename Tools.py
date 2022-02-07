9f17eb45b0d0d799fe5e37024e7baecc5b0b124a
CameronByer <chunecake@gmail.com>
2022-02-07 12:47:17 -0500

    Add Tools file
    Create a Tools file for various background work
    Includes:
    - Line class
    - mergelists function

---
+++ Tools.py
@@ -0,0 +1,29 @@
+class Line:
+    def __init__(self, p1, p2):
+        if p1[0] == p2[0]:
+            self.xint == p1[0]
+            self.yint == None
+            self.slope = None
+        else:
+            self.slope = (p2[1]-p1[1])/(p2[0]-p1[0])
+            self.yint = p1[1]-p1[0]*self.slope
+            self.xint = self.getx(0)
+    def getx(self, y):
+        if self.slope == 0:
+            return None
+        if self.slope == None:
+            return self.xint
+        return (y-self.yint)/self.slope
+    def gety(self, x):
+        if self.slope == None:
+            return None
+        return self.yint+x*self.slope
+
+def mergelists(a, b):
+    merged = []
+    while a!=[] and b!=[]:
+        if a[0]<b[0]:
+            merged.append(a.pop(0))
+        else:
+            merged.append(b.pop(0))
+    return merged+a+b
